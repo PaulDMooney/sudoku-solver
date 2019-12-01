b1401e6f8b63b83a832f2bdb8e4fd33082b61559
Paul Mooney <paul.mooney@live.com>
2019-12-01 16:50:33 -0500

    Added board creation method

--- src/app/sudoku-structure/board.spec.ts
+++ src/app/sudoku-structure/board.spec.ts
@@ -1,5 +1,5 @@
 import { Cell } from './cell';
-import { Board } from './board';
+import { Board, boardFactory } from './board';
 import { CellContainer } from './cell-container';
 import { take } from 'rxjs/operators';
 
@@ -7,12 +7,11 @@ describe('Board', () => {
 
   it('should emit event when all containers solved', async (done) => {
 
-
     // Given
     const cellOptions = [1, 2, 3, 4];
     const boxCells = [new Cell(cellOptions), new Cell(cellOptions), new Cell(cellOptions), new Cell(cellOptions)];
     const rowCells = [boxCells[0], boxCells[1], new Cell(cellOptions), new Cell(cellOptions)];
-    const board = new Board([new CellContainer(boxCells), new CellContainer(rowCells)]);
+    const board = new Board([new CellContainer(boxCells), new CellContainer(rowCells)], []);
 
     // Expecting overlapping cell boxCell[0] to be set automatically, expecting final cell rowCells[3] to be set automatically
     boxCells[2].eliminateAllOptionsExcept(1);
@@ -28,4 +27,40 @@ describe('Board', () => {
     done();
 
   });
+});
+
+describe('BoardFactory', () => {
+  it ('should create a board with a 9 by 9 array of cells', () => {
+
+    // When
+    const board = boardFactory();
+
+    // Then
+    expect(board.grid.length).toBe(9);
+    board.grid.forEach((cells: Cell[]) => {
+      expect(cells.length).toBe(9);
+    });
+  });
+
+  it('should create a board with a grid of all unique cells', () => {
+
+    // When
+    const board = boardFactory();
+    const visitedCells: Cell[] = [];
+
+    // Then
+    [].concat(...board.grid).forEach(cell => {
+      expect(visitedCells).not.toContain(cell);
+      visitedCells.push(cell);
+    });
+  });
+
+  // it('should create a board with a cell-container for each row in the grid', () => {
+
+  //   // When
+  //   const board = boardFactory();
+
+  //   // Then
+  //   // ??? How do I test for this when we don't keep the structure... yet??
+  // });
 });
--- src/app/sudoku-structure/board.ts
+++ src/app/sudoku-structure/board.ts
@@ -1,11 +1,12 @@
 import { CellContainer } from './cell-container';
 import { ReplaySubject, Observable, Subject, forkJoin, combineLatest, BehaviorSubject } from 'rxjs';
+import { Cell } from './cell';
 
 export class Board {
 
   boardSolved$: Subject<boolean> = new BehaviorSubject(false);
 
-  constructor(private cellContainers: CellContainer[]) {
+  constructor(private cellContainers: CellContainer[], public grid: Cell[][]) {
 
     combineLatest(cellContainers.map(cellContainer => cellContainer.containerSolvedEvent))
       .subscribe((statuses: boolean[]) => {
@@ -18,4 +19,69 @@ export class Board {
   get boardSolved(): Observable<boolean> {
     return this.boardSolved$;
   }
+}
+
+export function boardFactory(squareSize: number = 3): Board {
+
+  const dimensionLength = squareSize * squareSize;
+  const options: number[] = [];
+  for (let option = 1; option <= squareSize; option++) {
+    options.push(option);
+  }
+
+  // Add all of the cells into a 2 dimensional grid
+  const grid: Cell[][] = [];
+  for (let i = 0; i < dimensionLength; i++) {
+    grid.unshift([]);
+    for (let j = 0; j < dimensionLength; j++) {
+      grid[0].push(new Cell(options));
+    }
+  }
+
+  const rows = createCellContainersFromRows(grid);
+  const columns = createCellContainersFromColumns(grid);
+  const squares = createCellContainersFromSquares(grid, squareSize);
+
+  const toReturn = new Board([...rows, ...columns, ...squares], grid);
+  return toReturn;
+}
+
+function createCellContainersFromColumns(grid: Cell[][]): CellContainer[] {
+  return grid.map((column: Cell[]) => new CellContainer(column));
+}
+
+function createCellContainersFromRows(grid: Cell[][]): CellContainer[] {
+  const toReturn = [];
+  for (let i = 0; i < grid.length; i++) {
+    const row = [];
+    for (let j = 0; j < grid[i].length; j++) {
+      row.push(grid[j][i]);
+    }
+    toReturn.push(new CellContainer(row));
+  }
+  return toReturn;
+}
+
+function createCellContainersFromSquares(grid: Cell[][], squareSize: number): CellContainer[] {
+
+  const toReturn = [];
+
+  for (let xSquares = 0; xSquares < squareSize; xSquares++) {
+    for (let ySquares = 0; ySquares < squareSize; ySquares++) {
+      const cells = getSquare(grid, xSquares, ySquares, squareSize);
+    }
+  }
+
+  return toReturn;
+}
+
+function getSquare(grid: Cell[][], xSquare: number, ySquare: number, squareSize: number) {
+
+  const cells = [];
+  for (let i = xSquare * squareSize; i < xSquare * squareSize + squareSize; i++) {
+    for (let j = xSquare * squareSize; j < xSquare * squareSize + squareSize; j++) {
+      cells.push(grid[i][j]);
+    }
+  }
+  return cells;
 }
